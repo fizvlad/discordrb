2419412272b60fc201d5aad3b86de9d0927c3293
Fizvlad <fizvlad@mail.ru>
2020-01-22 11:11:38 +0300

    Close websocket on error

--- lib/discordrb/websocket.rb
+++ lib/discordrb/websocket.rb
@@ -49,7 +49,7 @@ module Discordrb
           end
         end
         ws.on(:close) { |err| instance.close_handler.call(err) }
-        ws.on(:error) { |err| instance.error_handler.call(err) }
+        ws.on(:error) { |err| instance.error_handler.call(err); close }
       end
     end
 
